dcd3df9aa26398ab06bb9141c1974e1f0033aae2
Ahmet <cyb3rpunk2099@gmail.com>
2019-05-08 01:32:36 +0300

    bookmanager code added

--- src/exercise1/start/BookClient/BookClient/Data/BookManager.cs
+++ src/exercise1/start/BookClient/BookClient/Data/BookManager.cs
@@ -1,35 +1,76 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+#pragma warning disable RCS1090 // Call 'ConfigureAwait(false)'.
 
 namespace BookClient.Data
 {
     public class BookManager
     {
-        public Task<IEnumerable<Book>> GetAll()
+        private const string Url = "http://bookserver2914.azurewebsites.net/api/books/";
+        private string authorizationKey;
+
+        private async Task<HttpClient> GetClient()
         {
-            // TODO: use GET to retrieve books
-            throw new NotImplementedException();
+            var client = new HttpClient();
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync(Url + "login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
         }
 
-        public Task<Book> Add(string title, string author, string genre)
+        public async Task<IEnumerable<Book>> GetAll()
         {
-            // TODO: use POST to add a book
-            throw new NotImplementedException();
+            var client = await GetClient();
+            string result = await client.GetStringAsync(Url);
+            return JsonConvert.DeserializeObject<IEnumerable<Book>>(result);
         }
 
-        public Task Update(Book book)
+        public async Task<Book> Add(string title, string author, string genre)
         {
-            // TODO: use PUT to update a book
-            throw new NotImplementedException();
+            var book = new Book()
+            {
+                Title = title,
+                Authors = new List<string>(new[] { author }),
+                ISBN = string.Empty,
+                Genre = genre,
+                PublishDate = DateTime.Now.Date,
+            };
+
+            var client = await GetClient();
+            var response = await client.PostAsync(Url,
+               new StringContent(
+                     JsonConvert.SerializeObject(book),
+                     Encoding.UTF8, "application/json"));
+
+            return JsonConvert.DeserializeObject<Book>(
+               await response.Content.ReadAsStringAsync());
         }
 
-        public Task Delete(string isbn)
+        public async Task Update(Book book)
         {
-            // TODO: use DELETE to delete a book
-            throw new NotImplementedException();
+            var client = await GetClient();
+            await client.PutAsync(Url + "/" + book.ISBN,
+               new StringContent(
+                     JsonConvert.SerializeObject(book),
+                     Encoding.UTF8, "application/json"));
+        }
+
+        public async Task Delete(string isbn)
+        {
+            var client = await GetClient();
+            await client.DeleteAsync(Url + isbn);
         }
     }
 }
 
+#pragma warning restore RCS1090 // Call 'ConfigureAwait(false)'.
